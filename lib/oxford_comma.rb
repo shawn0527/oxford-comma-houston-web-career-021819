231f5bd0df0cc7bbce9763f5ba18b075e2133710
Shawn Ren <doggy-ren@hotmail.com>
2019-02-18 02:45:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join(" and ")
-  elsif array.length == 3
-    array[-1].prepend("and ")
+  else
+    array[-1].prepend "and "
     array.join(", ")
   end
 end
